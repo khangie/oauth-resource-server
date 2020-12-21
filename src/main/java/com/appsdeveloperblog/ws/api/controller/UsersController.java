2f13fe2922628894018addc73b7ec2b917d95e55
= <khangie@hotmail.com>
2020-12-21 07:52:01 -0800

    Added @PostAuthorize

--- src/main/java/com/appsdeveloperblog/ws/api/controller/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/controller/UsersController.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.ws.api.controller;
 
+import com.appsdeveloperblog.ws.api.response.UserRest;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -23,6 +25,14 @@ public class UsersController {
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable("id") final String id, @AuthenticationPrincipal Jwt jwt) {
         return "Deleted user with id=" + id + " and JWT subject = " + jwt.getSubject();
+    }
+
+    // returnObject is a default keyword that refers to the return value of the method
+    // Hard code sub from JWT token which is the user_id
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path = "/{id}")
+    public UserRest getUser(@PathVariable("id") final String id, @AuthenticationPrincipal Jwt jwt) {
+        return new UserRest("John", "Doe", "bedb6f0e-cdab-4cda-b55e-c51e15d2c68d");
     }
 
 }
